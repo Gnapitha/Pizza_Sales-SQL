44ed5cd2cb638cd0266c6fee902c0286c77a704e
Gnapitha <91693381+Gnapitha@users.noreply.github.com>
2024-05-26 01:13:48 +0530

    Add files via upload

---
+++ File8.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT category , count(name) from pizza_types
+group by category
+LIMIT 0, 1000
+
+-- Date: 2024-05-26 01:09
+*/
+INSERT INTO `` (`category`,`count(name)`) VALUES (Chicken,6);
+INSERT INTO `` (`category`,`count(name)`) VALUES (Classic,8);
+INSERT INTO `` (`category`,`count(name)`) VALUES (Supreme,9);
+INSERT INTO `` (`category`,`count(name)`) VALUES (Veggie,9);
