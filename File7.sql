2e3c60dfaef5f68a78bdf0c0096878519bf27445
Gnapitha <91693381+Gnapitha@users.noreply.github.com>
2024-05-26 01:13:10 +0530

    Add files via upload

---
+++ File7.sql
@@ -0,0 +1,23 @@
+/*
+-- Query: SELECT HOUR(order_time) as hour, count(order_id) as order_count
+from orders
+group by hour(order_time)
+LIMIT 0, 1000
+
+-- Date: 2024-05-26 01:04
+*/
+INSERT INTO `` (`hour`,`order_count`) VALUES (11,1231);
+INSERT INTO `` (`hour`,`order_count`) VALUES (12,2520);
+INSERT INTO `` (`hour`,`order_count`) VALUES (13,2455);
+INSERT INTO `` (`hour`,`order_count`) VALUES (14,1472);
+INSERT INTO `` (`hour`,`order_count`) VALUES (15,1468);
+INSERT INTO `` (`hour`,`order_count`) VALUES (16,1920);
+INSERT INTO `` (`hour`,`order_count`) VALUES (17,2336);
+INSERT INTO `` (`hour`,`order_count`) VALUES (18,2399);
+INSERT INTO `` (`hour`,`order_count`) VALUES (19,2009);
+INSERT INTO `` (`hour`,`order_count`) VALUES (20,1642);
+INSERT INTO `` (`hour`,`order_count`) VALUES (21,1198);
+INSERT INTO `` (`hour`,`order_count`) VALUES (22,663);
+INSERT INTO `` (`hour`,`order_count`) VALUES (23,28);
+INSERT INTO `` (`hour`,`order_count`) VALUES (10,8);
+INSERT INTO `` (`hour`,`order_count`) VALUES (9,1);
