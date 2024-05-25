0b0d0f34d60a5ee71f2db482b65d971cff4dc370
Gnapitha <91693381+Gnapitha@users.noreply.github.com>
2024-05-26 01:11:57 +0530

    Add files via upload

---
+++ File3.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT pizza_types.name,pizzas.price
+FROM pizza_types join pizzas on pizza_types.pizza_type_id = pizzas.pizza_type_id
+order by pizzas.price desc
+limit 1
+-- Date: 2024-05-26 01:08
+*/
+INSERT INTO `` (`name`,`price`) VALUES ('The Greek Pizza',35.95);
